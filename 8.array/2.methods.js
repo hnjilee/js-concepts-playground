877430637718cfcac2d09f499c829ef399630516
hnjilee <hnjilee@gmail.com>
2026-02-15 09:35:49 +0900

    Add array examples (reference, methods)

---
+++ 8.array/2.methods.js
@@ -0,0 +1,10 @@
+// map
+const doubled = [1, 2, 3].map(n => n * 2);
+console.log(doubled); // [ 2, 4, 6 ]
+
+// filter
+const even = [1, 2, 3, 4].filter(n => n % 2 === 0);
+console.log(even); // [ 2, 4 ]
+
+// forEach
+[1, 2, 3].forEach(n => console.log(n)); // 1, 2, 3
