d88b3fd75228d50a2f5d1646dc554507f12cab80
hnjilee <hnjilee@gmail.com>
2026-02-17 19:04:35 +0900

    Add module examples (export and import)

---
+++ 10.modules/app.js
@@ -0,0 +1,6 @@
+// import로 필요한 코드를 가져옴
+
+import { add, MIN_NUMBER } from './math.js';
+
+console.log(add(1, 2)); // 3
+console.log(MIN_NUMBER); // 0
---
+++ 10.modules/math.js
@@ -0,0 +1,7 @@
+// export로 외부에 공개
+
+export function add(a, b) {
+  return a + b;
+}
+
+export const MIN_NUMBER = 0;
